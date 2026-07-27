2de1dd51c9328475118dd65e2ba5fc4f5eae4a5a
Aethertenshi <hasan.ahmadibrahim@gmail.com>
2026-07-27 17:47:22 +0800

    Chore: Add scripts/ directory to .gitignore and unstage local build artifacts

--- scripts/build-openal/build/win-x64/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/ProjectTemp-Server/OpenRei-cs/scripts/build-openal/downloads/openal-soft-1.25.2
-# Build directory: E:/ProjectTemp-Server/OpenRei-cs/scripts/build-openal/build/win-x64
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("fmt-11.2.0")
--- scripts/build-openal/build/win-x64/fmt-11.2.0/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/ProjectTemp-Server/OpenRei-cs/scripts/build-openal/downloads/openal-soft-1.25.2/fmt-11.2.0
-# Build directory: E:/ProjectTemp-Server/OpenRei-cs/scripts/build-openal/build/win-x64/fmt-11.2.0
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
