c82a95e2d86b4e9c24ce7578462d5a937b39df74
Aethertenshi <hasan.ahmadibrahim@gmail.com>
2026-07-28 20:34:03 +0800

    Fix: Add F1-F12, Windows/Super, system keys to KeyType and change MapKeycode fallback from Space to Unknown

--- OpenRei/Input/KeyType.cs
+++ OpenRei/Input/KeyType.cs
@@ -5,6 +5,8 @@ namespace OpenRei.InputSystem;
 /// </summary>
 public enum KeyType
 {
+    Unknown,
+
     // Mouse Buttons
     MouseLeft,
     MouseRight,
@@ -26,13 +28,31 @@ public enum KeyType
     N, O, P, Q, R, S, T, U, V, W, X, Y, Z,
     Num0, Num1, Num2, Num3, Num4, Num5, Num6, Num7, Num8, Num9,
 
-    // Modifiers
+    // Function Keys
+    F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12,
+
+    // Modifiers & System
     LeftShift,
     RightShift,
     LeftControl,
     RightControl,
     LeftAlt,
     RightAlt,
+    LeftSuper,  // Windows / Command key Left
+    RightSuper, // Windows / Command key Right
+    CapsLock,
+    NumLock,
+    ScrollLock,
+
+    // Navigation & Editing
+    Insert,
+    Delete,
+    Home,
+    End,
+    PageUp,
+    PageDown,
+    PrintScreen,
+    Pause,
 
     // Gamepad
     GamepadA,
